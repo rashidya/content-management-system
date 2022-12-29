86d2089e2dc3d10d2bada339e47ba0e10f42ee38
rashidya <rashmiliyanage2000@gmail.com>
2022-12-29 22:18:29 +0530

    node modules removed

--- content-management-system-backend/src/configs/db.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    database :{
-        host: 'localhost',
-        user: 'root',
-        password: '1234',
-        database: 'contentManagementSystem',
-        insecureAuth : true
-    }
-}
